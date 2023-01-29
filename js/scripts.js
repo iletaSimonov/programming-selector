8cfbe0bba2d5f40daa20872086dc6ab95562255e
Ileta Simonov <ileta.simonov@yahoo.com>
2023-01-28 17:09:18 -0800

    adds a function to hide results

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 function hideResults() {
   document.getElementById("error-message").setAttribute("class", "hidden");
-  document.getElementById("channing-tatum").setAttribute("class", "hidden");
-  document.getElementById("jenna-ortega").setAttribute("class", "hidden");
-  document.getElementById("pete").setAttribute("class", "hidden");
-  document.getElementById("em-rata").setAttribute("class", "hidden");
-  document.getElementById("sorry").setAttribute("class", "hidden");
+  document.getElementById("pet").setAttribute("class", "hidden");
+  document.getElementById("outdoor").setAttribute("class", "hidden");
+  document.getElementById("food").setAttribute("class", "hidden");
+  document.getElementById("travel").setAttribute("class", "hidden");
+  document.getElementById("super-power").setAttribute("class", "hidden");
+  document.getElementById("c#").setAttribute("class", "hidden");
+  document.getElementById("ruby").setAttribute("class", "hidden");
+  document.getElementById("python").setAttribute("class", "hidden");
+  document.getElementById("swift").setAttribute("class", "hidden");
 }
