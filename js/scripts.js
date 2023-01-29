3789ef13e5b1b562dee4d366d0a7a9437229307c
Ileta Simonov <ileta.simonov@yahoo.com>
2023-01-28 16:43:35 -0800

    adds 3 questions to survey the best language for the user

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function hideResults() {
+  document.getElementById("error-message").setAttribute("class", "hidden");
+  document.getElementById("channing-tatum").setAttribute("class", "hidden");
+  document.getElementById("jenna-ortega").setAttribute("class", "hidden");
+  document.getElementById("pete").setAttribute("class", "hidden");
+  document.getElementById("em-rata").setAttribute("class", "hidden");
+  document.getElementById("sorry").setAttribute("class", "hidden");
+}
