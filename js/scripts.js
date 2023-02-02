d2393817e8e3ed020f7d0164292eda316a88fc06
Ileta Simonov <ileta.simonov@yahoo.com>
2023-02-01 19:40:53 -0800

    adds event listeners and form handlers. Simplified by removing some scripts so that  only one of the questions form a response. WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,37 @@
-window.onload = function(event) {
+function hideResults() {
+  document.getElementById("python").setAttribute("class", "hidden");
+  document.getElementById("c").setAttribute("class", "hidden");
+  document.getElementById("ruby").setAttribute("class", "hidden");
+};
+
+window.onload = function handleForm(event) {
   event.preventDefault();
+  const userSelections = document.querySelectorAll("input[name=user-input]:checked");
   const form = document.querySelector('form');
-  form.addEventListener('submit', function(event) {
-    
+  form.addEventListener('submit', function() {
+     event.preventDefault();
     let pet = document.querySelector('input[name="pet"]:checked').value;
     let python = document.getElementById('python');
     let c = document.getElementById('c#');
     let ruby = document.getElementById('ruby');
     
     if (pet === 'cat') {
-      python.removeAttribute('class');
+      document.getElementById("python").removeAttribute('class');
     } else if (pet === 'dog') {
-      c.removeAttribute('class');
+      document.getElementById("c").removeAttribute('class');
     } else (pet === 'hamster'); 
-      ruby.removeAttribute('class');
-  }
+      document.getElementById("ruby").removeAttribute('class');
+  };
+
+  window.addEventListener("load", function {
+    document.querySelector("form#survey").addEventListener("submit", handleForm);
+  });
+
+  $('submit').click(function() {
+    $('form').hide();
+    return false;
+});
+
 )};
+
+
